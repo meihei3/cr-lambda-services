e811cbe39e1d8e534a8c685ab63496244bf10fdb
yameholo <ame.aroid.xz5@gmail.com>
2020-11-29 03:55:10 +0900

    update main program

--- handler.py
+++ handler.py
@@ -1,24 +1,67 @@
 import json
+import os
+import requests
+from typing import Dict
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+BASE_URL = 'https://api.clashroyale.com/v1'
+ACCESS_KEY = os.environ['CR_ACCESS_KEY']
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
 
-    return response
+def init_headers() -> Dict[str, str]:
+    """
+    初期化されたヘッダー情報の辞書を返す
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
+    Returns
+    -------
+    dict
+        header情報
     """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        'content-type': 'application/json; charset=utf-8',
+        'cache-control': 'max-age=60',
+        'authorization': f'Bearer {ACCESS_KEY}',
     }
+
+
+def get_member(clan_tag: str) -> Dict[str, str]:
     """
+    クラロワAPIからclan_tagのメンバー情報をGETする
+
+    Parameters
+    ----------
+    clan_tag : str
+        クランタグ (ex. #228UCY92)
+
+    Returns
+    -------
+    dict
+        APIのレスポンス
+    """
+    url = BASE_URL + f'/clans/{clan_tag}/members'
+    headers = init_headers()
+
+    res = requests.get(url, headers=headers)
+
+    return res.json()
+
+
+def lambda_function(event, context) -> Dict[str, str]:
+    """
+    実行用の関数
+
+    Parameters
+    ----------
+    event : list
+        入力パラメータ
+    context : list
+        よくわからん
+
+    Returns
+    -------
+    data : dict
+        結果
+    """
+    data = get_member('#228UCY92')
+
+    return data
